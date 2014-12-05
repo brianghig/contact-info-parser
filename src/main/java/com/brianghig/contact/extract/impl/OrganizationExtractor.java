3f799376e617d54d8384b4434f07e428c5f90408
Brian Ghigiarelli <brianghig@gmail.com>
2014-12-04 23:32:21 -0500

    Adding implementations for Name and Organization extraction

--- src/main/java/com/brianghig/contact/extract/impl/OrganizationExtractor.java
+++ src/main/java/com/brianghig/contact/extract/impl/OrganizationExtractor.java
@@ -1,6 +1,10 @@
 package com.brianghig.contact.extract.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.brianghig.contact.extract.Extractor;
 import com.brianghig.contact.model.ContactInfo;
@@ -8,7 +12,58 @@ import com.brianghig.contact.model.ContactInfo;
 public class OrganizationExtractor implements Extractor {
 
 	public List<String> extract(List<String> lines, ContactInfo contact) {
-		return lines;
+		
+		List<String> remainingLines = new ArrayList<String>();
+		
+		/*
+		 * Loop backwards through the lines so we can remove
+		 * them along the way.
+		 */
+		lineLoop: for(int i = lines.size() - 1; i >= 0; i--) {
+			
+			String line = lines.remove(i);
+			
+			boolean isOrg = isOrganization(line);
+			
+			if( isOrg ) {
+				contact.setOrganization(line);
+				// Add all of the remaining lines since we found our match
+				remainingLines.addAll(lines);
+				break lineLoop;
+			}
+			else {
+				remainingLines.add(line);
+			}
+		}
+		
+		return remainingLines;
+		
 	}
 
+	private boolean isOrganization(String line) {
+		String[] tokens = line.split("[^\\w']+");
+		for(String token : tokens) {
+			if(ORG_TOKENS.contains(token.toLowerCase())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
+	private static final String[] ORG_GAZATEER = {
+		"technologies",
+		"inc",
+		"llc",
+		"incorporated",
+		"limited liability corporation",
+		"ltd"
+		//TODO Load this from a gazateer file with a fuller dictionary
+	};
+	
+	private static final Set<String> ORG_TOKENS = new HashSet<String>();
+	static {
+		ORG_TOKENS.addAll(Arrays.asList(ORG_GAZATEER));
+	}
+	
 }
